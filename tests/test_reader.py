2ddafb082d938e06616ecbb08053387c124e6a35
Yaroslav Bondarev <dev.bondarev@gmail.com>
2020-11-19 18:32:03 +0300

    fix test path

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -5,7 +5,7 @@ import json
 def test_get_dict_from_file_json() -> None:
     reader: Reader = Reader()
     result: dict = reader.get_dict_from_file(
-        "/home/user/PycharmProjects/jsonvalidator/tests/test_data/event/1eba2aa1-2acf-460d-91e6-55a8c3e3b7a3.json")
+        "./tests/test_data/event/1eba2aa1-2acf-460d-91e6-55a8c3e3b7a3.json")
     expected_result_str: str = '{"event": "label_selected", "data": {"id": null, "rr_id": null, "labels": [{"slug": ' \
                                '"flu", "type": 2, "color": {"color": "#e83e35", "label": "stress"}, "name_en": ' \
                                '"cold/flu", "name_ru": ' \
@@ -22,7 +22,7 @@ def test_get_dict_from_file_json() -> None:
 def test_get_dict_from_dict_scheme() -> None:
     reader: Reader = Reader()
     result: dict = reader.get_dict_from_file(
-        "/home/user/PycharmProjects/jsonvalidator/tests/test_data/schema/cmarker_created.schema")
+        "./tests/test_data/schema/cmarker_created.schema")
     expected_result_str: str = '{"type": "object", "$schema": "http://json-schema.org/schema#", "required": [' \
                                '"cmarkers", "datetime", "user_id"], "properties": {"user_id": {"type": "integer"}, ' \
                                '"cmarkers": {"type": "array", "items": {"type": ["object", "string"], "required": [' \
